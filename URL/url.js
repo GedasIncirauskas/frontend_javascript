fbf5974897a50265046aa93f715b3a5038c49323
GedasIncirauskas <gedas.inc@gmail.com>
2021-09-30 18:37:52 +0300

    init

--- URL/url.js
+++ URL/url.js
@@ -1,5 +1,6 @@
-export const registerURL = "http://localhost:3000/v1/auth/register";
-export const loginURL = "http://localhost:3000/v1/auth/login";
-export const groupURL = "http://localhost:3000/v1/accounts";
-export const billsURL = "http://localhost:3000/v1/bills";
+const url = "https://backend-nodejs-c8f42.ondigitalocean.app/bacend";
+export const registerURL = `${url}/v1/auth/register`;
+export const loginURL = `${url}/v1/auth/login`;
+export const groupURL = `${url}/v1/accounts`;
+export const billsURL = `${url}/v1/bills`;
 export const token = localStorage.getItem("token");
